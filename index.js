ddc8f032a425cbfd2bcc3835ee0acf2a0209d8b6
Srinath Patnam <N01495266@humbermail.ca>
2022-03-23 01:15:29 -0400

    Authentication

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+
+const connectDB = require('./config/connectDB');
+
+const userRoute = require('./routes/userRoutes');
+const authRoute = require('./routes/authRoutes');
+const app = express();
+
+app.use(cors());
+
+//connect to db
+connectDB();
+
+//middleware work with incoming request
+//set the middleware to parse the data
+app.use(express.json());
+ 
+//router to organize our routes
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+const PORT = process.env.PORT | 5000;
+app.listen(PORT, 'localhost', () => {
+  console.log('Server is running');
+});
