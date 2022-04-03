1ec8843ae590df2287c1e349310c2ea291c4962a
Jasveer Kaur <dhaliwaljass585@gmail.com>
2022-04-03 17:55:57 -0400

    raise connected to database

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const cors = require('cors');
+//const cors = require('cors');
 
 const connectDB = require('./config/connectDB');
 
+const raiseRoute = require('./routes/raiseRoutes');
 const userRoute = require('./routes/userRoutes');
 const authRoute = require('./routes/authRoutes');
+
+
 const app = express();
 
-app.use(cors());
+//app.use(cors());
 
 //connect to db
 connectDB();
@@ -18,8 +21,11 @@ connectDB();
 app.use(express.json());
  
 //router to organize our routes
+app.use('/api/raises', raiseRoute);
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
+
+
 const PORT = process.env.PORT | 5000;
 app.listen(PORT, 'localhost', () => {
   console.log('Server is running');
---
+++ routes/raiseRoutes.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const {check,validationResult}= require ('express-validator')
+
+let Raise = require('../models/Raise')
+const authMiddleware=require('../middlewares/authMiddleware');
+const router = express.Router();
+
+//route Get api/raise
+//desc Get all raise
+//access public
+router.get(
+  '/',
+authMiddleware,
+ async (req, res) => {
+    try {      
+         const raise= await Raise.find();
+       // console.log(raise);
+        res.send(raise);
+    } catch (error) {
+        res.status(500).send('server error');
+    }
+  
+});
+
+//route Get api/raise/:id
+//desc Get raise by id
+//access public
+router.get('/:id', async(req, res) => {
+  
+    try {
+        const findRaise= await Raise.findById(req.params.id);
+        if (!findRaise) {
+            return res.status(404).send('Raise not found');
+          } else {
+            res.send(findRaise);
+          }
+    } catch (error) {
+        res.status(500).send('Server Error')
+    }   
+});
+
+//route Post api/raise
+//desc Add a raise
+//access public
+router.post(
+  '/',
+ authMiddleware,
+  [
+    check('title','Title is required').not().isEmpty(),
+    check('description','Description need to be at least 30 chars').isLength({ min: 30, }),
+] , 
+  async(req, res) => {
+  try {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors:errors.array()});
+    }
+      const newRaise = await Raise.create({
+        user:req.user.id,
+        title: req.body.title,
+        description: req.body.description,
+       })
+  } catch (error) {
+    //console.log(error);
+    return res.status(500).json({ error:'server error' });
+  }
+ res.send('New Raise added');
+});
+
+//route Delete api/raise/
+//desc delete raise by id
+//access public
+router.delete(
+  '/:id', 
+  authMiddleware,
+  async(req, res) => {
+    try {      
+        const delRaise= await Raise.findOneAndRemove({_id: req.params.id});
+        if (!delRaise) {
+            return res.status(404).send('Raise not found');
+          } else {
+            res.send("Raise deleted");
+          }
+    } catch (error) {
+        res.status(500).send('Server Error')
+    } 
+});
+
+//route PUT api/raise/
+//desc update raise by id
+//access public
+router.put(
+  '/',
+  authMiddleware, 
+  async(req, res) => {
+  //find the element
+try {
+    const updateRaise =await Raise.findById(req.body.id)
+    if (!updateRaise) {
+        return res.status(404).send('Raise not found');
+      }   
+      //console.log(updateRaise);
+      updateRaise.title= req.body.title;
+      updateRaise.description= req.body.description;
+            
+      await updateRaise.save();
+      res.send (updateRaise);    
+} catch (error) {
+  return res.status(500).json({ error: error.message });
+}
+  
+});
+
+module.exports = router;
